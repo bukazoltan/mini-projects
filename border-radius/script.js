8c388757b7c5861e1f178be2d29f285318f6acc9
Zoltan Buka <bukazoltan@gmail.com>
2020-02-26 14:39:50 +0100

    Implemented one-line output feature.

--- border-radius/script.js
+++ border-radius/script.js
@@ -1,14 +1,17 @@
 const sliders = document.querySelectorAll('.slider');
 const box = document.querySelector('.box');
 const textarea = document.querySelector('.textbox');
-const successText = document.querySelector('.copypaste>p')
+const oneLine = document.querySelector('.one-line');
+const successText = document.querySelector('.copypaste>p');
 
 const updateTextbox = () => {
     let lines = []
     sliders.forEach(slider => {
         lines.push(`border-${slider.id}-radius: ${slider.value}px;`);
     })
-    textarea.value = lines.join("\n")
+    textarea.value = lines.join("\n");
+    oneLine.value = `border-radius: ${sliders[0].value}px ${sliders[1].value}px ${sliders[2].value}px ${sliders[3].value}px;`;
+    console.log(oneLine);
 };
 
 updateTextbox();
@@ -19,9 +22,10 @@ const handleSlider = (e) => {
     updateTextbox();
 };
 
-const handleTextbox = () => {
-    textarea.select();
-    textarea.setSelectionRange(0, 99999); /*For mobile devices*/
+const handleTextbox = (e) => {
+    console.log(e);
+    e.target.select();
+    e.target.setSelectionRange(0, 99999); /*For mobile devices*/
     document.execCommand("copy");
     successText.classList.remove('invisible');
     successText.classList.add('success-text');
@@ -30,4 +34,6 @@ const handleTextbox = () => {
 
 textarea.addEventListener('click', handleTextbox);
 textarea.addEventListener('selectionchange', handleTextbox);
+oneLine.addEventListener('click', handleTextbox);
+oneLine.addEventListener('selectionchange', handleTextbox);
 sliders.forEach(slider => slider.addEventListener('input', handleSlider));
