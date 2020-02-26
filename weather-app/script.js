8165eadc35b7d234097d3f16d09a62d89f7a2470
Zoltán Buka <bukazoltan@gmail.com>
2020-02-26 20:25:08 +0100

    removed info

--- weather-app/script.js
+++ weather-app/script.js
@@ -4,6 +4,6 @@ const btn = document.querySelector('.searchBtn');
 
 
 async function getWeather() {
-  const response = await fetch('http://api.weatherstack.com/current?access_key=6b236b011fcc1ef180f7285df7ef1ca9&query=budapest')
-  return response.json();     
+    const response = await fetch('url');
+    return response.json();
 };
